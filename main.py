f213228900094c060dc092a55142ef825df2a372
dimaffffff <97465037+dimaffffff@users.noreply.github.com>
2026-06-23 00:09:18 +0100

    remade the tileset class, deleted array3D

--- main.py
+++ main.py
@@ -1,47 +1,27 @@
 import pygame
 import os
-import random
+import math
 
 pygame.init()
 
 #classes
-class Array3D:
-    def __init__(self,width,height):
-        self.array = []
-        for y in range(int(height)):
-            self.array.append([])
-            for x in range(int(width)):
-                self.array[y].append([])
-        self.width = width
-        self.height = height
-
-    def getItems(self,pos:tuple):
-        return self.array[pos[1]][pos[0]]
-    
-    def addItem(self,pos:tuple,item):
-        self.array[pos[1]][pos[0]].append(item)
-
-    def setItems(self,pos:tuple,value):
-        self.array[pos[1]][pos[0]] = value
-
-    def __str__(self):
-        return str(self.array)
-    
-class TileSet(Array3D):
-        def __init__(self, surface: pygame.Surface,size:tuple):       
+class TileSet():
+        def __init__(self, surface: pygame.Surface,tileSize:int):       
             self.surface = surface
             self.surfaceSize = self.surface.get_size()
-            self.tileAmountX = size[0]
-            self.tileAmountY = size[1]
-            self.tileSpacingX = int(self.surfaceSize[0] / self.tileAmountX)
-            self.tileSpacingY = int(self.surfaceSize[1] / self.tileAmountY)
-            super().__init__(self.tileAmountX,self.tileAmountY)
+            self.tileSize = tileSize
 
-        def getPos(self,tile: tuple):
-            return (tile[0]*self.tileSpacingX, tile[1]*self.tileSpacingY)
+        def getPosFromTile(self,tile: tuple):
+            return tuple([axis * self.tileSize for axis in tile])
         
+        def getTileFromPos(self,pos:tuple):
+            tile = [axis / self.tileSize for axis in pos]
+            tile = [math.floor(i) for i in tile]
+            return tile
+        
+
 class SpritesBase(pygame.sprite.Sprite):
-    def __init__(self,image,x,y,width,height,groups = []):
+    def __init__(self,image,x,y,width,height,groups = ()):
         super().__init__(*groups)
         self.original_image = image
         self.image = pygame.transform.scale(pygame.image.load(os.path.join(os.path.dirname(os.path.abspath(__file__)),self.original_image)),(width,height))
@@ -52,21 +32,20 @@ class SpritesBase(pygame.sprite.Sprite):
 
 
 class Sprites(SpritesBase):
-    def __init__(self,image,tileSet: TileSet,tilePos:tuple,groups = ()):
-        pos = (tilePos[0] * tileSet.tileSpacingX, tilePos[1] * tileSet.tileSpacingY)
-        size = (tileSet.tileSpacingX,tileSet.tileSpacingY)
-        tileSet.addItem(tilePos,self)
+    def __init__(self,image,tileSet: TileSet,tilePos:tuple,groups: tuple = ()):
+        pos = tileSet.getPosFromTile(tilePos)
+        size = (tileSet.tileSize,tileSet.tileSize)
         super().__init__(image,pos[0],pos[1],size[0],size[1],groups)
 
 
 spritesGroup = pygame.sprite.Group()
-
-game_window = pygame.display.set_mode((800,800))
+info = pygame.display.Info()
+game_window = pygame.display.set_mode((info.current_w,info.current_h), pygame.RESIZABLE)
 
 pygame.display.set_caption("Game Window")
 
 #objects
-gameTileSet=TileSet(game_window,(10,10))
+gameTileSet=TileSet(game_window,48)
 player = Sprites("assets/player.png",gameTileSet,(4,4),(spritesGroup,))
 
 #variables
